fc2720283759331d36f11914133c1cbc629680ce
rileuy <126115841+rileuy@users.noreply.github.com>
2023-06-07 14:05:20 -0700

    Update SimonSays_Final_DEMO.ino

--- SimonSays_Final_DEMO/SimonSays_Final_DEMO.ino
+++ SimonSays_Final_DEMO/SimonSays_Final_DEMO.ino
@@ -1,61 +1,108 @@
+//Riley Fitzsimonds
+//ENGR 103 Final
+//Simon Says - Memory Game
+
 #include <Adafruit_CircuitPlayground.h>
 
 volatile bool switchState = 0;
 volatile bool switchFlag = 0;
+volatile bool startB = false;
+volatile bool resetB = false;
 int score;
 float level;
-
+int sequence[100];
+int sequenceLength = 0;
 
 void setup() {
+  CircuitPlayground.begin();
+  CircuitPlayground.clearPixels(); // Clear pixels on startup
 
-pinmode (4, INPUT_PULLDOWN); //start button = 7
-pinmode (5, INPUT_PULLDOWN); // reset game button = 5
-pinmode (7, IMPUT_PULLUP); // Display current or highest score in LED form. will choose one. 
+  pinMode(4, INPUT_PULLUP);   // start button = 4
+  pinMode(5, INPUT_PULLUP);   // reset game button = 5
+  pinMode(7, INPUT_PULLUP);   // Display current or highest score in LED form. Will choose one.
 
-attachInterrupt(digitalPinToInterrupt(4), start, RISING);
-attachInterrupt(digitalPinToInterrupt(5), reset, RISING);
- attachInterrupt(digitalPinToInterrupt(7), score, CHANGE);
+  attachInterrupt(digitalPinToInterrupt(4), start, RISING);
+  attachInterrupt(digitalPinToInterrupt(5), reset, RISING);
+  attachInterrupt(digitalPinToInterrupt(7), showScore, CHANGE);
 
-randomSeed(analogRead(0));
+  randomSeed(analogRead(0));
 
   switchState = digitalRead(7);
 
-Serial.print("Score: "); // print score
-
-
+  Serial.begin(9600); 
+  Serial.print("Score: "); // print score
+  Serial.println(score);
 }
 
 void loop() {
-if(score) {
-
-
+  if (startB) {
+    startB = false;
+    resetB = false;
+    score = 1;
+    simonGame();
+  }
+  
+  if (resetB) {
+    resetB = false;
+    sequenceLength = 0;
+    score = 0;
+  }
 }
 
+void simonGame() {
+  if (sequenceLength == 0) {
+    addToSequence();
+  }
+
+  for (int i = 0; i < sequenceLength; i++) {
+    int button = sequence[i];
+    switch (startB) {
+      case 0:
+        CircuitPlayground.setPixelColor(0, 255, 0, 0);  // Red
+        delay(500);
+        CircuitPlayground.clearPixels();
+        delay(200);
+        break;
+      case 1:
+        CircuitPlayground.setPixelColor(1, 0, 0, 255);  // Blue
+        delay(500);
+        CircuitPlayground.clearPixels();
+        delay(200);
+        break;
+      case 2:
+        CircuitPlayground.setPixelColor(2, 0, 255, 0);  // Green
+        delay(500);
+        CircuitPlayground.clearPixels();
+        delay(200);
+        break;
+      case 3:
+        CircuitPlayground.setPixelColor(3, 255, 255, 0);  // Yellow
+        delay(500);
+        CircuitPlayground.clearPixels();
+        delay(200);
+        break;
+    }
+    delay(300);
+  }
+
+  addToSequence();
 }
 
-void buttonsTouched() {
-  uint8_t 
-}
-
-void simonGame(){
-case 1:
-
-
-}
-void currentScore() {
-
+void addToSequence() {
+  int nextButton = random(4);
+  sequence[sequenceLength] = nextButton;
+  sequenceLength++;
 }
 
 void start() {
-  buttonA = 1;
+  startB = true;
 }
 
 void reset() {
-  buttonB = 1;
+  resetB = true;
 }
 
-void score() {
-  delay(50); 
-    switchFlag = true;
-  
+void showScore() {
+  delay(50);
+  switchFlag = true;   // Show current or highest score using LEDs
 }
