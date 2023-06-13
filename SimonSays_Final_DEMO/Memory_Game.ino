3a31de8510199e4a6af24c42738611f5bca172b7
rileuy <126115841+rileuy@users.noreply.github.com>
2023-06-12 23:50:13 -0700

    Update Memory_Game.ino

--- SimonSays_Final_DEMO/Memory_Game.ino
+++ SimonSays_Final_DEMO/Memory_Game.ino
@@ -12,75 +12,79 @@ void setup() {
   CircuitPlayground.begin();
   Serial.begin(9600);
 
-  pinMode(7, INPUT_PULLDOWN);
+  pinMode(7, INPUT_PULLUP);
 
   switchState = CircuitPlayground.slideSwitch();
   delay(1000);
 }
 
 void loop() {
-
   switchState = CircuitPlayground.slideSwitch();
   rCounter = 0;
   lCounter = 0;
-  
-  if(!lost) 
-  {
+
+  if (!lost) {
     int i;
     buttonPress = false;
 
-    for(i=0; i < difficulty; i++)
-    {
+    for (i = 0; i < difficulty; i++) {
       currNum = GenState();
-      if (currNum == 0){
+      if (currNum == 0) {
         rCounter++;
-      }
-      else {
+        CircuitPlayground.strip.setPixelColor(9, 255, 0, 0); // Turn on the LED on the right side (NeoPixel index 9)
+        CircuitPlayground.redLED(true); // Turn on the red LED
+        CircuitPlayground.strip.show(); // Update the NeoPixel LEDs
+        delay(500); // Adjust the delay as needed
+        CircuitPlayground.strip.setPixelColor(9, 0); // Turn off the LED
+        CircuitPlayground.redLED(false); // Turn off the red LED
+        CircuitPlayground.strip.show(); // Update the NeoPixel LEDs
+      } else {
         lCounter++;
+        CircuitPlayground.strip.setPixelColor(0, 0, 0, 255); // Turn on the LED on the left side (NeoPixel index 0)
+        CircuitPlayground.redLED(true); // Turn on the blue LED
+        CircuitPlayground.strip.show(); // Update the NeoPixel LEDs
+        delay(500); // Adjust the delay as needed
+        CircuitPlayground.strip.setPixelColor(0, 0); // Turn off the LED
+        CircuitPlayground.redLED(false); // Turn off the blue LED
+        CircuitPlayground.strip.show(); // Update the NeoPixel LEDs
       }
-
       delay(200);
     }
+
+    Serial.print("Left Counter: ");
     Serial.println(lCounter);
+    Serial.print("Right Counter: ");
     Serial.println(rCounter);
 
-    if(rCounter == lCounter) {
+    if (rCounter == lCounter) {
       rCounter++;
     }
 
-    while(!buttonPress)
-    {
-      if(CircuitPlayground.rightButton() && !buttonPress)
-      {
+    while (!buttonPress) {
+      if (CircuitPlayground.rightButton() && !buttonPress) {
         buttonPress = true;
-        if(rCounter > lCounter){
+        if (rCounter > lCounter) {
           ++difficulty;
           delay(50);
+        } else {
+          lost = true;
+        }
       }
-      else{
-        lost = true;
-      }
-      }
-    if(CircuitPlayground.leftButton() && !buttonPress)
-    {
-      buttonPress = true;
-      if(lCounter > rCounter){
-        ++difficulty;
-        delay(50);
-      }
-      else{
-        lost = true;
+      if (CircuitPlayground.leftButton() && !buttonPress) {
+        buttonPress = true;
+        if (lCounter > rCounter) {
+          ++difficulty;
+          delay(50);
+        } else {
+          lost = true;
+        }
       }
     }
-    }
-
   }
-
 }
 
 int GenState() {
   int randNum;
-  randNum = random(0,2);
-
+  randNum = random(0, 2);
   return randNum;
 }
